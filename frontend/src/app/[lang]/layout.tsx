adf697abf1821a3fd487945c3aca793cc24eafed
anthonyhook <anthonyhook55@gmail.com>
2024-04-22 12:14:27 +1000

    Fixed image

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import { getStrapiMedia, getStrapiURL } from "./utils/api-helpers";
 import { fetchAPI } from "./utils/fetch-api";
 
 import { i18n } from "../../../i18n-config";
+import Script from 'next/script';
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -78,6 +79,7 @@ export default async function RootLayout({
 
   return (
     <html lang={params.lang}>
+     
       <body>
         <div className="w-full h-screen tint flex flex-col md:flex-row">
           <div className="hidden md:block md:w-3/12 w-full image-tint">
@@ -91,7 +93,7 @@ export default async function RootLayout({
           <div className="md:w-5/12 w-full pl-4 md:pl-20 flex flex-col justify-end" style={{ height: '100%' }}>
             <div style={{ marginBottom: '25%' }}>
               <Image
-                src="http://localhost:1337/uploads/Icon_3e2c39791e.svg"
+                src="https://graceful-talent-f765bfd7e2.media.strapiapp.com/Icon_3e2c39791e.svg"
                 width={60}
                 height={60}
                 alt="Codx Partners"
@@ -102,6 +104,9 @@ export default async function RootLayout({
               </p>
 
               <p className="text-white font-bold pt-6">Coming soon</p>
+
+              <div data-mooform-id="41ac37d3-a615-4e97-a220-e76c0786ab07" ></div>
+
               <p className="text-white font-light pt-6">
                 <a href="mailto:hello@codxpartners.com">hello@codxpartners.com</a>
               </p>
